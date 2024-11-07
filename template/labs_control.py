42bc77d3deb740c3082cb07611c8fe2c5788ed7d
Amaroq <nike-marie@hotmail.de>
2024-11-07 11:25:53 +0100

    adds lab1 template src code to repo

---
+++ template/labs_control.py
@@ -0,0 +1,308 @@
+import dotenv
+import subprocess
+import signal
+import random
+import requests
+import threading
+import functools
+import math
+import time
+
+class LabsControl:
+    def __init__(self, base_path):
+        self.base_path = base_path
+        dotenv_path = base_path + '/.env'
+        self.env = dotenv.dotenv_values(
+            dotenv_path=dotenv_path
+        )
+
+        self.sp = None
+
+    def build(self):
+        return subprocess.run("docker build -t ds_labs_server {}/server".format(self.base_path), shell=True, check=True).returncode == 0
+
+
+    def start_scenario(self, num_servers=1, scenario='perfect', scenario_timeout=0):
+
+        if self.sp is not None:
+            return
+        self.num_servers = num_servers
+        self.sp = subprocess.Popen("python3 {}/labs.py {} {} {}".format(self.base_path, num_servers, scenario, scenario_timeout), shell=True, stdin=subprocess.PIPE, encoding='utf-8')
+
+    def change_scenario(self, s):
+        if self.sp:
+            self.sp.stdin.write(s + "\n")
+            self.sp.stdin.flush()
+
+    def get_server_list(self, num_servers=1):
+        server_list = ["localhost:" + str(int(self.env['BASE_SERVER_PORT']) + i) for i in range(num_servers)]
+        return server_list
+
+    def shutdown(self):
+        if self.sp:
+            self.sp.send_signal(signal.SIGINT)
+            self.sp.wait()
+            self.sp = None
+
+
+class ServerManager():
+    def __init__(self, server_list):
+        self.server_list = server_list
+
+    def req(self, srv_id, URI, req='POST', data=None, timeout_s=30):
+        srv_ip = self.server_list[srv_id]
+        success = False
+        try:
+            if 'POST' in req:
+                # We handle data string as json for now
+                headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+                res = requests.post('http://{}{}'.format(srv_ip, URI), data=data, headers=headers,
+                                    timeout=timeout_s)
+            elif 'GET' in req:
+                headers = {'Accept': 'application/json'}
+                res = requests.get('http://{}{}'.format(srv_ip, URI), headers=headers, timeout=timeout_s)
+                # result can be accessed res.json()
+            else:
+                res = None
+
+            if res is not None and res.status_code == 200:
+                success = True
+        except Exception as e:
+            print("[ERROR] " + str(e))
+            res = None
+        return (success, res)
+
+    def post(self, srv_id, URI, data=None, timeout_s=30):
+        return self.req(srv_id, URI, 'POST', data, timeout_s)
+
+    def get(self, srv_id, URI):
+        return self.req(srv_id, URI, 'GET', None)
+
+    def status(self, id):
+        s, r = self.req(id, "/status", 'GET')
+        if s:
+            return r.json()
+        else:
+            return None
+
+class CrashManager():
+    def __init__(self, sm : ServerManager):
+        self.sm = sm
+        self.crashed_servers = set() # no server crashed in the beginning
+
+    def crash_server(self, id):
+        self.sm.post(id, "/crash")
+        self.crashed_servers.add(id)
+
+    def recover_server(self, id):
+        self.sm.post(id, "/recover")
+        self.crashed_servers.remove(id)
+
+    def get_alive_ids(self):
+        return [i for i in range(len(self.sm.server_list)) if i not in self.crashed_servers]
+
+    def get_random_uncrashed_server(self):
+        alive_ids = self.get_alive_ids()
+        if len(alive_ids) == 1:
+            return None
+        else:
+            return random.choice(alive_ids)
+
+    def get_random_crashed_server(self):
+        if len(self.crashed_servers) == 0:
+            return None
+        else:
+            return random.choice(list(self.crashed_servers))
+
+    def crash_random_server(self):
+        id = self.get_random_uncrashed_server()
+        if id is not None:
+            self.crash_server(id)
+            return True
+        else:
+            return False
+
+    def recover_random_server(self):
+        id = self.get_random_crashed_server()
+        if id is not None:
+            self.recover_server(id)
+            return True
+        else:
+            return False
+
+class EntryManager:
+    def __init__(self, sm : ServerManager):
+        self.sm = sm
+
+    def handle(self, callbacks, in_parallel=False):
+        res = None
+        if in_parallel:
+            res = [None for cb in callbacks]
+            threads = []
+            for i, cb in enumerate(callbacks):
+                def wrapped_cb(idx, cb):
+                    res[idx] = cb()
+
+                threads.append(threading.Thread(target=functools.partial(wrapped_cb, i, cb), args=()))
+
+            for t in threads:
+                t.start()
+
+            for t in threads:
+                t.join()
+        else:
+            res = [
+                cb() for cb in callbacks
+            ]
+        return res
+
+    def add_entry_to_sever(self, server_id, value):
+        return self.sm.post(server_id, '/entries', data={'value': str(value)})
+
+    def get_entries_from_server(self, server_id):
+        return self.sm.get(server_id, '/entries')
+
+    def add_entries(self, count, servers, parallel=False):
+        def add_entries_cb(server):
+            for i in range(count):
+                self.add_entry_to_sever(server, random.randint(0, 1000000))
+
+        callbacks = [functools.partial(add_entries_cb, server) for server in servers]
+        return self.handle(callbacks, parallel)
+
+class DSLTestControl():
+
+    def __init__(self, lc: LabsControl):
+        self.lc = lc
+
+    def init_servers(self, count):
+        print(f"initing {count} servers")
+        self.lc.shutdown()
+        self.lc.start_scenario(num_servers=count, scenario_timeout=0)
+
+        self.sm = ServerManager(self.lc.get_server_list(count))
+        self.cm = CrashManager(self.sm)
+        self.em = EntryManager(self.sm)
+
+    def parse_server_args(self, servers):
+        if servers == 'all' or servers == '100%':
+            return list(range(self.lc.num_servers))
+        # if servers is just a number, it is an absolute number of random servers
+        elif isinstance(servers, str) and servers[-1] == '%':
+            # select a random subset of nodes
+            num_servers = math.ceil(float(servers[:-1]) / self.lc.num_servers)
+            return random.sample(range(self.lc.num_servers), num_servers)
+        elif isinstance(servers, list):
+            # the servers are already given as a list, we just remove duplicates
+            uniq = []
+            for s in servers:
+                s = int(s)
+                if s not in uniq:
+                    uniq.append(s)
+            return uniq
+        elif isinstance(servers, int):
+            # servers is a single number
+            num_servers = int(servers)
+            return random.sample(range(self.lc.num_servers), num_servers)
+        else:
+            raise ValueError(f"Invalid server argument {servers}")
+
+    def change_scenario(self, name):
+        print(f"Scenario {name}")
+        self.lc.change_scenario(name)
+
+    def crash_servers(self, servers):
+        print(f"Crashing servers {servers}")
+        for s in self.parse_server_args(servers):
+            self.cm.crash_server(s)
+
+    def recover_servers(self, servers):
+        print(f"Recovering servers {servers}")
+
+        for s in self.parse_server_args(servers):
+            self.cm.recover_server(s)
+
+    def assertion_online(self, servers):
+        print(f"assertion_online servers {servers}")
+        return all([self.sm.status(s) is not None for s in self.parse_server_args(servers)])
+
+    def assertion_equal(self, percentage, servers):
+        print(f"assertion_equal servers {servers}")
+
+        all_hashes = [self.sm.status(s)['hash'] for s in range(self.lc.num_servers)]
+
+        if percentage == 'all':
+            percentage = '100%'
+
+        num_required = min(self.lc.num_servers, math.ceil(int(percentage[:-1])*0.01 * self.lc.num_servers))
+
+        suc = True
+        for s in self.parse_server_args(servers):
+            ref_hash = all_hashes[s]
+
+            if all_hashes.count(ref_hash) < num_required:
+                suc = False
+                break
+
+        print(f"assertion_equal result {suc}")
+        return suc
+
+    def assertion_equal_length(self, percentage, servers):
+        print(f"assertion_equal_length servers {servers}")
+
+        all_lens = [self.sm.status(s)['hash'] for s in range(self.lc.num_servers)]
+
+        if percentage == 'all':
+            percentage = '100%'
+
+        num_required = min(self.lc.num_servers, math.ceil(int(percentage[:-1]) * 0.01 * self.lc.num_servers))
+
+        for s in self.parse_server_args(servers):
+            ref_len = all_lens[s]
+
+            if all_lens.count(ref_len) < num_required:
+                return False
+        return True
+
+    def wait_until(self, assertion):
+        print(f"Waiting until {assertion}")
+        start_time = time.time()
+        while not assertion():
+            #print("Sleeping!")
+            time.sleep(0.1) # sleep for 100 ms to not overwhelm the servers...
+        end_time = time.time()
+        print(f"Waited for {end_time - start_time:.2f} seconds")  # todo Log time!
+
+    def waits(self, secs):
+        print(f"Wait for {secs} seconds")
+        time.sleep(secs)
+
+    def log_assertion(self, start_time, end_time, succ):
+        print(f"Assertion took {end_time - start_time:.2f} seconds")
+        print(f"Assertion {'succeeded' if succ else 'failed'}")
+
+    def add_entries(self, count, servers, parallel=False):
+        print(f"Adding {count} entries to servers {servers} {'in parallel' if parallel else ''}")
+        self.em.add_entries(count, self.parse_server_args(servers), parallel)
+
+    def update_entries(self, entries, servers, parallel=False):
+        print(f"Updating {entries} of entries on servers {servers} {'in parallel' if parallel else ''}")
+        print("Not yet implemented")
+        # NOT YET IMPLEMENTED, sorry!
+
+if __name__ == "__main__":
+
+    lc = LabsControl('.')
+    try:
+        lc.build()
+        lc.start_scenario(num_servers=2, scenario_timeout=5)
+        import time
+        time.sleep(10) # we need to wait until the initial scenario timed out!
+        lc.change_scenario('easy')
+        time.sleep(5)
+        lc.change_scenario('off')
+        time.sleep(5)
+        lc.change_scenario('easy')
+        time.sleep(30)
+    finally:
+        lc.shutdown()
---
+++ template/server/server.py
@@ -0,0 +1,304 @@
+# coding=utf-8
+from bottle import Bottle, request, HTTPError, run, response
+
+from paste import httpserver
+
+import threading
+import os
+import time
+import json
+import hashlib
+
+import requests
+import functools
+
+# A simple entry in our board
+class Entry:
+    def __init__(self, id, value):
+        self.id = id
+        self.value = value
+
+    def to_dict(self) -> dict:
+        return {
+            "id": self.id,
+            "value": self.value
+        }
+
+    def from_dict(data: dict):
+        return Entry(data['id'], data['value'])
+
+    def __str__(self):
+        return str(self.to_dict())
+
+# ------------------------------------------------------------------------------------------------------
+# You need to synchronize the access to the board when you use multithreading in the server (e.g. using a Lock)
+class Board():
+
+    def __init__(self):
+        self.indexed_entries = {} # indexed_entries is defined on initialization
+
+    def add_entry(self, entry):                 # entry is passed to function
+        self.indexed_entries[entry.id] = entry  # entry is added to board's own indexed_entry list by id
+
+    def get_ordered_entries(self):
+        ordered_indices = sorted(list(self.indexed_entries.keys()))
+        return [self.indexed_entries[k] for k in ordered_indices]
+
+# ------------------------------------------------------------------------------------------------------
+class Server(Bottle):
+
+    def __init__(self, ID, IP, server_list):
+        super(Server, self).__init__()
+        self.id = int(ID)
+        self.ip = str(IP)
+        self.server_list = server_list
+
+        self.status = {
+            "crashed": False,
+            "notes": "",
+            "num_entries": 0, # we use this to generate ids for the entries
+        }
+
+        self.lock = threading.RLock() # use reentry lock for the server
+
+        # Handle CORS
+        self.route('/<:re:.*>', method='OPTIONS', callback=self.add_cors_headers)
+        self.add_hook('after_request', self.add_cors_headers)
+
+        # Those two http calls simulate crashes, i.e., unavailability of the server
+        self.post('/crash', callback=self.crash_request)
+        self.post('/recover', callback=self.recover_request)
+        self.get('/status', callback=self.status_request)
+
+        # Define REST URIs for the frontend (note that we define multiple update and delete routes right now)
+        self.post('/entries', callback=self.create_entry_request)
+        self.get('/entries', callback=self.list_entries_request)
+        self.put('/entries/<entry_id>', callback=self.update_entry_request)     
+        self.delete('/entries/<entry_id>', callback=self.delete_entry_request)
+
+        # REST URIs for the later algorithms
+        self.post('/message', callback=self.message_request)                # a new message request is sent to server
+
+        self.board = Board()
+
+    # Please try to avoid modifying the following methods
+    # ------------------------------------------------------------------------------------------------------
+    def add_cors_headers(self):
+        """
+        You need to add some headers to each request.
+        Don't use the wildcard '*' for Access-Control-Allow-Origin in production.
+        """
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
+    def list_entries_request(self):
+        # DONT use me for server to server stuff as this is also available when crashed. Please implement another method for that (which also handles the crashed state!)
+        try:
+
+            with self.lock:
+                ordered_entries = self.board.get_ordered_entries()
+                dict_entries = list(map(lambda entry: entry.to_dict(), ordered_entries))
+
+                return {
+                    "entries": dict_entries,
+                    "server_status": {
+                        "len": len(ordered_entries),
+                        "hash": hashlib.sha256((json.dumps(tuple(dict_entries)).encode('utf-8'))).hexdigest(),
+                        "crashed": self.status["crashed"],
+                        "notes": self.status["notes"]
+                    }  # we piggyback here allowing for a simple frontend implementation
+                }
+        except Exception as e:
+            print("[ERROR] " + str(e))
+            raise e
+
+    def status_request(self):
+        # DONT use me for server to server stuff as this is also available when crashed. Please implement another method for that (which also handles the crashed state!)
+        try:
+
+            with self.lock:
+                ordered_entries = self.board.get_ordered_entries()
+                dict_entries = list(map(lambda entry: entry.to_dict(), ordered_entries))
+                return {
+                    "len": len(ordered_entries),
+                    "hash": hashlib.sha256((json.dumps(tuple(dict_entries)).encode('utf-8'))).hexdigest(),
+                    "crashed": self.status["crashed"],
+                    "notes": self.status["notes"]
+                }
+        except Exception as e:
+            print("[ERROR] " + str(e))
+            raise e
+
+    def crash_request(self):
+        try:
+            if not self.status["crashed"]:
+                self.status["crashed"] = True
+        except Exception as e:
+            print("[ERROR] " + str(e))
+            raise e
+
+    def recover_request(self):
+        try:
+            if self.status["crashed"]:
+                self.status["crashed"] = False
+        except Exception as e:
+            print("[ERROR] " + str(e))
+            raise e
+
+    # This route is called whenever there is a new message for this server, you should handle everything in self.handle_message
+    # is called whenever 
+    def message_request(self):
+        try:
+            if self.status["crashed"]:
+                # we ignore this message
+                response.status = 408
+                return None
+            try:
+                # Please modify handle_message to return a response
+                return self.handle_message(request.json)
+            except Exception as e:
+                print("[ERROR] " + str(e))
+                return None
+        except Exception as e:
+            print("[ERROR] " + str(e))
+            raise e
+
+    # This method sends a message to another server
+    # Note that it will not send a message when the server is crashed
+    # Do not modify this method if not necessary
+    def _send_message(self, srv_ip, message):
+        if self.status["crashed"]:
+            return False  # when we are crashed we do not send messages
+
+        success = False
+        data = None
+        res = None
+
+        # We handle data string as json for now
+        # We always POST this message
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+        try:
+            res = requests.post('http://{}/message'.format(srv_ip), data=json.dumps(message), headers=headers,
+                                timeout=1)  # timeout should stay at 1 sec
+
+            # result can be accessed res.json()
+            if res.status_code == 200:
+                data = res.json()
+            if res.status_code == 200 or res.status_code == 204:
+                success = True
+        except Exception as e:
+            print("[ERROR] " + str(e))
+
+        return (success, data, res)
+
+# You can modify the following methods as you wish
+# ------------------------------------------------------------------------------------------------------
+
+    def create_entry_request(self):
+        try:
+            if self.status["crashed"]:
+                response.status = 408
+                return
+
+            entry_value = request.forms.get('value')
+            with self.lock:
+                # no lock needed here
+                pass
+
+            self.send_message(self.server_list[0], {'type': 'add_entry', 'entry_value': entry_value})
+
+            return {}
+        except Exception as e:
+            print("[ERROR] " + str(e))
+            raise e
+
+    def update_entry_request(self, entry_id):
+        try:
+            if self.status["crashed"]:
+                response.status = 408
+                return
+
+            entry_value = request.forms.get('value')
+            with self.lock:
+                print("Updating entry with id {} to value {}".format(entry_id, entry_value))
+                # TODO: not implemented atm!
+            return {}
+        except Exception as e:
+            print("[ERROR] " + str(e))
+            raise e
+
+    def delete_entry_request(self, entry_id):
+        try:
+            if self.status["crashed"]:
+                response.status = 408
+                return
+
+            with self.lock:
+                print("Deleting entry with id {}".format(entry_id))
+                # TODO: not implemented atm!
+
+            return {}
+        except Exception as e:
+            print("[ERROR] " + str(e))
+            raise e
+
+
+
+    def send_message(self, srv_ip, message):
+        # TODO: Implement your custom code here
+        # - What if the request gets lost? (Task 3)
+        # - What if the request is delayed? (Task 3)
+        # - What if the response gets lost? (optional)
+        return self._send_message(srv_ip, message)
+
+    # This method is called whenever a message is received
+    # Note that this function call will block one of the NUM_THREADS threads that handle the web server
+    # so if you intend to block for a long time, you should spawn a new thread, for example (but be careful with locking)
+    # We provide a basic implementation for the coordinator:
+    #   - If a server wants to add a new entry, it will send an 'add_entry' message to the coordinator
+    #   - The coordinator will propagate it to all other servers (including itself)
+    #   - If a server receives a 'propagate' message, it will add the entry to the board
+    def handle_message(self, message):
+        # Note that you might need to use the lock
+        print("Received message: ", message)
+
+        if 'type' in message:
+            type = message['type']
+
+            if type == 'add_entry':
+
+                assert self.id == 0 # ID 0 is coordinator only this server should receive add_entry messages right now
+                entry_value = message['entry_value']
+
+                # We can safely propagate here since we always have a single frontend client, right? So no need to lock, right? Right?!
+                for other in self.server_list:
+                    # TODO: Send message to other servers concurrently?
+                    self.send_message(other, {'type': 'propagate', 'entry_value': entry_value})
+
+            elif type == 'propagate':
+                entry_value = message['entry_value']
+                # Let's hope this is from the coordinator
+                with self.lock:
+                    self.status['num_entries'] += 1
+                    entry = Entry(self.status['num_entries'], entry_value)
+                    self.board.add_entry(entry)
+            else:
+                print("Received weird message?")
+
+        return {}
+
+# Sleep a bit to allow logging to be attached
+time.sleep(2)
+
+# the server_list contains all server ips of the distributed blackboard
+server_list = os.getenv('SERVER_LIST').split(',')
+own_id = int(os.getenv('SERVER_ID'))
+own_ip = server_list[own_id]
+
+server = Server(own_id, own_ip, server_list)
+
+NUM_THREADS = 10
+print("#### Starting Server {} with {} threads".format(str(own_id), NUM_THREADS))
+httpserver.serve(server, host='0.0.0.0', port=80, threadpool_workers=NUM_THREADS,
+                 threadpool_options={"spawn_if_under": NUM_THREADS})
